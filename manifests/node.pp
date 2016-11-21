ae9bbd72f21b35abc5e215c6945c0af2b6107956
kumarsenthil <kumar.senthilgs@gmail.com>
2016-11-21 19:20:16 +0530

    updating node file for iis

--- manifests/node.pp
+++ manifests/node.pp
@@ -1,13 +1,27 @@
 node "dev-test-com" {
   
   notify{ "This is COOL":}
+
+   iis::manage_app_pool {'application_pool':
+    enable_32_bit           => true,
+    managed_runtime_version => 'v4.0',
+  }
+
+   iis::manage_site {'dev.test.com':
+    site_path     => 'C:\inetpub\wwwroot\mysite',
+    site_id       => '10'
+    port          => '80',
+    ip_address    => '*',
+    host_header   => 'dev.test.com',
+    app_pool      => 'my_application_pool'
+  }
 	
 }
 
-node "qa-rightslogic-com" {
+node "qa-test-com" {
 	
 }
 
-node "www-rightslogic-com" {
+node "www-test-com" {
 	
 }
