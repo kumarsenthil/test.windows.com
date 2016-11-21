f284860a4b5361fbb5c93ff846690475a62be272
kumarsenthil <kumar.senthilgs@gmail.com>
2016-11-21 15:58:29 +0530

    updating node file

--- manifests/node.pp
+++ manifests/node.pp
@@ -1,11 +1,13 @@
-node dev.rightslogic.com {
+node "dev-test-com" {
+  
+  notify{ "This is COOL":}
 	
 }
 
-node qa.rightslogic.com {
+node "qa-rightslogic-com" {
 	
 }
 
-node www.rightslogic.com {
+node "www-rightslogic-com" {
 	
 }
