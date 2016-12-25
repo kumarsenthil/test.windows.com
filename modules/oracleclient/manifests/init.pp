449abb2d04dc08d7479e62c2de1e0ed7c45bbb9e
kumarsenthil <kumar.senthilgs@gmail.com>
2016-12-25 19:25:26 +0530

    updated

--- modules/oracleclient/manifests/init.pp
+++ modules/oracleclient/manifests/init.pp
@@ -1,7 +1,7 @@
 class oracleclient {
 	
 	exec { "Download Oracle client":
-      command   => "https://s3.amazonaws.com/test-dev-cdn-watch-aetnd-com/Tensorflow.docx;Start-Sleep -s 300",
+      command   => "Invoke-WebRequest  https://s3.amazonaws.com/test-dev-cdn-watch-aetnd-com/Tensorflow.docx -OutFile C:\\test.docx;Start-Sleep -s 300",
       provider  => powershell,
       logoutput => true,
     }
