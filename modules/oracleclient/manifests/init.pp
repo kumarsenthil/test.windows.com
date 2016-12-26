e9ae20d8e435035be91993fd8b6cd87d9239de72
kumarsenthil <kumar.senthilgs@gmail.com>
2016-12-26 18:57:08 +0530

    Fixed syntax issue

--- modules/oracleclient/manifests/init.pp
+++ modules/oracleclient/manifests/init.pp
@@ -3,7 +3,7 @@ class oracleclient {
 	exec { "Download JRE":
       command   => "Invoke-WebRequest https://s3.amazonaws.com/testdemo4321/jdk1.8.0_111.zip -OutFile C:\\jdk1.8.0_111.zip;Start-Sleep -s 60",
       provider  => powershell,
-      creates 	=> "C:\\jdk1.8.0_111.zip"
+      creates 	=> "C:\\jdk1.8.0_111.zip",
       logoutput => true,
       before	=> Exec["Install JRE"],
     }
@@ -11,25 +11,25 @@ class oracleclient {
     exec { "Install JRE":
       command   => "Add-Type -assembly 'system.io.compression.filesystem';[io.compression.zipfile]::ExtractToDirectory('C:\\jdk1.8.0_111.zip', 'C:\\jdk1.8.0_111')",
       provider  => powershell,
-      creates 	=> "C:\\jdk1.8.0_111.zip"
+      creates 	=> "C:\\jdk1.8.0_111.zip",
       logoutput => true,
-      require	=> Exec["Download JRE"]
+      require	=> Exec["Download JRE"],
     }
 	
 	exec { "Download Oracle client":
       command   => "Invoke-WebRequest https://s3.amazonaws.com/testdemo4321/win64_11gR2_client.zip -OutFile C:\\win64_11gR2_client.zip;Start-Sleep -s 600",
       provider  => powershell,
-      creates 	=> "C:\\win64_11gR2_client.zip"
+      creates 	=> "C:\\win64_11gR2_client.zip",
       logoutput => true,
-      require	=> Exec["Install JRE"]
+      require	=> Exec["Install JRE"],
     }
 
     exec { "Extract the Oracle client":
       command   => "Add-Type -assembly 'system.io.compression.filesystem';[io.compression.zipfile]::ExtractToDirectory('C:\\win64_11gR2_client.zip', 'C:\\');Start-Sleep -s 300",
       provider  => powershell,
       logoutput => true,
-      creates 	=> "C:\\client"
-      require	=> Exec["Download Oracle client"]
+      creates 	=> "C:\\client",
+      require	=> Exec["Download Oracle client"],
     }
 
     file { "deploy response file":
@@ -38,8 +38,8 @@ class oracleclient {
 	    group   => 'Administrator',
 	    source  => "puppet:///modules/oracleclient/responsefile.rsp",
 	    backup  => false,
-	    require	=> Exec["Extract the Oracle client"]
-	    before	=> Exec["Install Oracle client"]
+	    require	=> Exec["Extract the Oracle client"],
+	    before	=> Exec["Install Oracle client"],
 	}    
 
     exec { "Install Oracle client":
@@ -47,8 +47,8 @@ class oracleclient {
       provider  => powershell,
       cwd		=> "C:\\client"
       logoutput => true,
-      creates 	=> "C:\\client"
-      require	=> File["deploy response file"]
+      creates 	=> "C:\\client",
+      require	=> File["deploy response file"],
     }
 
 }
