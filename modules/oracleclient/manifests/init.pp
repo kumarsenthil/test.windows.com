efb007efcd5d75ceac24ce6addb838f093c43b22
kumarsenthil <kumar.senthilgs@gmail.com>
2016-12-28 11:31:56 +0530

    Fixing welcome window issue

--- modules/oracleclient/manifests/init.pp
+++ modules/oracleclient/manifests/init.pp
@@ -33,7 +33,7 @@ class oracleclient {
     }
 
     file { "deploy response file":
-	    path    => 'C:\\client\\responsefile.rsb',
+	    path    => 'C:\\client\\responsefile.rsp',
 	    owner   => 'Administrator',
 	    group   => 'Administrator',
 	    source  => "puppet:///modules/oracleclient/responsefile.rsp",
@@ -43,7 +43,7 @@ class oracleclient {
 	}    
 
     exec { "Install Oracle client":
-      command   => "setup -jreLoc C:\\jdk1.8.0_111 -responseFile C:\\client\\responsefile.rsp -silent ;Start-Sleep -s 300",
+      command   => "setup -jreLoc C:\\jdk1.8.0_111 -silent -nowelcome –nowait -responseFile C:\\client\\responsefile.rsp ;Start-Sleep -s 300",
       provider  => powershell,
       cwd		=> "C:\\client",
       logoutput => true,
