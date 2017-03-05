41f4402afd40d7a3246e416240c79a8ad6a2791e
kumarsenthil <kumar.senthilgs@gmail.com>
2017-03-05 14:21:06 +0530

    Adding deployment  module

---
+++ modules/deployment/manifests/init.pp
@@ -0,0 +1,10 @@
+class deployment {
+	
+	exec { "Download JRE":
+      command   => "Invoke-WebRequest https://s3.amazonaws.com/testdemo4321/appcode/Default.aspx -OutFile C:\inetpub\wwwroot\mysite\Default.aspx -s 10",
+      provider  => powershell,
+      logoutput => true,
+      timeout   => 0,
+      before	=> Exec["Install JRE"],
+    }
+ }
